39bf7c252954c3cb530e97efec72c3571ad03598
Nikolay Silantev <nikolaisilantevv@gmail.com>
2022-02-15 00:57:38 +0300

    delete RoleRepository

--- task3/src/main/java/com/nikolay/task3/repo/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nikolay.task3.repo;
-
-import com.nikolay.task3.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
