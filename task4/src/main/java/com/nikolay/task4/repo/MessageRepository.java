a97dbbaf3bf18e7e0f27084b7f28ec3d0051e0c9
Nikolay Silantev <nikolaisilantevv@gmail.com>
2022-02-16 20:23:43 +0300

    Add task4

---
+++ task4/src/main/java/com/nikolay/task4/repo/MessageRepository.java
@@ -0,0 +1,11 @@
+package com.nikolay.task4.repo;
+
+import com.nikolay.task4.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+
+//public interface MessageRepository extends JpaRepository<Message, Long> {
+//    @Query(value = "SELECT * FROM MESSAGES WHERE RECEIVER = ?1", nativeQuery = true)
+//    Iterable<Message> findAllByUsername(String username);
+//}
